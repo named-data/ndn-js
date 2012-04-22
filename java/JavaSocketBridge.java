9eaf41417bd8c07b31b43d0e33134afefc4b02bc
Meki Cherkaoui <meki.cherkaoui@gmail.com>
2012-04-22 15:32:12 -0700

    Replacing comment

--- java/JavaSocketBridge.java
+++ java/JavaSocketBridge.java
@@ -1,17 +1,5 @@
-// JavaSocketBridge.java
-// by Stephen Ware
-// April 25, 2009
-//
-// Part of the JavaSocketBridge project.
-// This applet provides an interface for using true sockets in JavaScript.
-//
-// Note: You will need to have the Java Plugin archive in your classpath to compile this.
-//       For me, that's C:\Program Files\Java\jre6\lib\plugin.jar
-// Note: You will need to jar this class and Listener.class into a signed jar file if
-//       you want your sockets to access domains other than the one this is running on.
-// Note: Apparently, when you grant permissions to Java applets in Java 6, you only grant
-//       them to the main applet thread.  That's the reason for all the confusing stuff
-//       in the connect methods... so that connections always happen on the main thread.
+// @author: Meki cherkaoui
+
 
 import java.applet.*;
 import javax.swing.*;
