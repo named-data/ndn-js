7665a2270ca0bec197eaef31fa7bab6175d18b94
Meki Cherkaoui <meki.cherkaoui@gmail.com>
2012-04-22 10:24:26 -0700

    Cleaning up

--- src-old/Name.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-var Name = function Name(_Component){
-	this.Component = _Component;
-};
-
-exports.Name = Name;
--- src-old/Signature.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-var Signature = function(_DigestAlgorithm, _Witness, _SignatureBits){
-	this.DigestAlgorithm = _DigestAlgorithm;
-	this.Witness = _Witness;
-	this.SignatureBits = _SignatureBits;
-}
-
-exports.Signature = Signature;
