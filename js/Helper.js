336fcb9e5c0c52dc5d6124e8aeb4b6ca29e69b75
Jeff Thompson <jeff@thefirst.org>
2012-11-11 19:23:34 -0800

    Add Helper.js

---
+++ js/Helper.js
@@ -0,0 +1,57 @@
+// Including js files to be used in the web pages
+// Add your js source code reference here, rather than in the HTML files
+
+document.write('<script type="text/javascript" src="../NDN.js"></script>');
+
+document.write('<script type="text/javascript" src="../util/CCNProtocolDTags.js"></script>');
+	
+document.write('<script type="text/javascript" src="../util/CCNTime.js"></script>');
+
+document.write('<script type="text/javascript" src="../Name.js"></script>');
+
+document.write('<script type="text/javascript" src="../ContentObject.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/DateFormat.js"></script>');
+
+document.write('<script type="text/javascript" src="../Interest.js"></script>');
+
+document.write('<script type="text/javascript" src="../Key.js"></script>');
+
+document.write('<script type="text/javascript" src="../PublisherID.js"></script>');
+
+document.write('<script type="text/javascript" src="../PublisherPublicKeyDigest.js"></script>');
+
+document.write('<script type="text/javascript" src="../FaceInstance.js"></script>');
+
+document.write('<script type="text/javascript" src="../ForwardingEntry.js"></script>');
+
+document.write('<script type="text/javascript" src="../Closure.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/BinaryXMLEncoder.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/BinaryXMLDecoder.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/BinaryXMLStructureDecoder.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/DataUtils.js"></script>');
+
+document.write('<script type="text/javascript" src="../encoding/EncodingUtils.js"></script>');
+                
+document.write('<script type="text/javascript" src="../security/KeyManager.js"></script>');
+
+document.write('<script type="text/javascript" src="../securityLib/jsbn.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/jsbn2.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/rsa.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/rsa2.js"></script>');
+
+document.write('<script type="text/javascript" src="../securityLib/sha1.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/sha256.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/sha512.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/md5.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/ripemd160.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/base64.js"></script>');
+
+document.write('<script type="text/javascript" src="../securityLib/rsapem-1.1.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/rsasign-1.2.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/asn1hex-1.1.js"></script>');
+document.write('<script type="text/javascript" src="../securityLib/x509-1.1.js"></script>');
