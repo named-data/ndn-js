35860050ee92c0228bd41b6cc160dac4376b00b9
Jeff Thompson <jefft0@gmail.com>
2013-09-26 16:03:12 -0700

    util: Update NameEnumeration to handle a big result with multiple segments.

--- js/util/name-enumeration.js
+++ js/util/name-enumeration.js
@@ -19,26 +19,28 @@ NameEnumeration.getComponents = function(ndn, prefix, onComponents)
   // Add %C1.E.be
   command.add([0xc1, 0x2e, 0x45, 0x2e, 0x62, 0x65])
   
-  ndn.expressInterest(command, new NameEnumeration.Closure(onComponents));
+  ndn.expressInterest(command, new NameEnumeration.Closure(ndn, onComponents));
 };
 
 /**
  * Create a closure for getting the response from the name enumeration command.
+ * @param {NDN} ndn The NDN object for using expressInterest.
  * @param {function} onComponents The onComponents callback given to getComponents.
  */
-NameEnumeration.Closure = function NameEnumerationClosure(onComponents) 
+NameEnumeration.Closure = function NameEnumerationClosure(ndn, onComponents) 
 {
   // Inherit from Closure.
   Closure.call(this);
   
+  this.ndn = ndn;
   this.onComponents = onComponents;
-  this.components = [];
+  this.contentParts = [];
 };
 
 /**
  * Parse the response from the name enumeration command and call this.onComponents.
- * @param {type} kind
- * @param {type} upcallInfo
+ * @param {number} kind
+ * @param {UpcallInfo} upcallInfo
  * @returns {Closure.RESULT_OK}
  */
 NameEnumeration.Closure.prototype.upcall = function(kind, upcallInfo) 
@@ -46,14 +48,42 @@ NameEnumeration.Closure.prototype.upcall = function(kind, upcallInfo)
   try {
     if (kind == Closure.UPCALL_CONTENT || kind == Closure.UPCALL_CONTENT_UNVERIFIED) {
       var data = upcallInfo.contentObject;
-    
-      NameEnumeration.addComponents(data.content, this.components);
+      
+      if (!NameEnumeration.endsWithSegmentNumber(data.name))
+        // We don't expect a name without a segment number.  Treat it as a bad packet.
+        this.onComponents(null);
+      else {
+        var segmentNumber = DataUtils.bigEndianToUnsignedInt
+            (data.name.components[data.name.components.length - 1]);
+        
+        // Each time we get a segment, we put it in contentParts, so its length follows the segment numbers.
+        var expectedSegmentNumber = this.contentParts.length;
+        if (segmentNumber != expectedSegmentNumber)
+          // Try again to get the expected segment.  This also includes the case where the first segment is not segment 0.
+          this.ndn.expressInterest
+            (data.name.getPrefix(data.name.components.length - 1).addSegment(expectedSegmentNumber), this);
+        else {
+          // Save the content and check if we are finished.
+          this.contentParts.push(data.content);
+          
+          if (data.signedInfo != null && data.signedInfo.finalBlockID != null) {
+            var finalSegmentNumber = DataUtils.bigEndianToUnsignedInt(data.signedInfo.finalBlockID);
+            if (segmentNumber == finalSegmentNumber) {
+              // We are finished.  Parse and return the result.
+              this.onComponents(NameEnumeration.parseComponents(DataUtils.concatArrays(this.contentParts)));
+              return;
+            }
+          }
+          
+          // Fetch the next segment.
+          this.ndn.expressInterest
+            (data.name.getPrefix(data.name.components.length - 1).addSegment(expectedSegmentNumber + 1), this);
+        }
+      }
     }
     else
       // Treat anything else as a timeout.
-      this.components = null;
-  
-    this.onComponents(this.components);
+      this.onComponents(null);
   } catch (ex) {
     console.log("NameEnumeration: ignoring exception: " + ex);
   }
@@ -62,13 +92,13 @@ NameEnumeration.Closure.prototype.upcall = function(kind, upcallInfo)
 };
 
 /**
- * Parse the content as a name enumeration response and add to components.  This makes a copy of the component.
+ * Parse the content as a name enumeration response and return an array of components.  This makes a copy of the component.
  * @param {Uint8Array} content The content to parse.
- * @param {Array<Uint8Array>} components 
- * @returns {undefined}
+ * @returns {Array<Uint8Array>} The array of components.
  */
-NameEnumeration.addComponents = function(content, components)
+NameEnumeration.parseComponents = function(content)
 {
+  var components = [];
   var decoder = new BinaryXMLDecoder(content);
   
   decoder.readStartElement(NDNProtocolDTags.Collection);
@@ -83,5 +113,18 @@ NameEnumeration.addComponents = function(content, components)
     decoder.readEndElement();  
   }
 
-  decoder.readEndElement();  
+  decoder.readEndElement();
+  return components;
 };
+
+/**
+ * Check if the last component in the name is a segment number.
+ * TODO: Move to Name class.
+ * @param {Name} name
+ * @returns {Boolean} True if the name ends with a segment number, otherwise false.
+ */
+NameEnumeration.endsWithSegmentNumber = function(name) {
+  return name.components != null && name.components.length >= 1 &&
+    name.components[name.components.length - 1].length >= 1 &&
+    name.components[name.components.length - 1][0] == 0;
+}
