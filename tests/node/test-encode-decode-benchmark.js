c5269d6dfbefa3d2df9f16ce7da31e1703ea6b9f
Jeff Thompson <jefft0@gmail.com>
2013-12-05 15:07:50 -0800

    tests: Added initial test-encode-decode-benchmark.

---
+++ tests/node/test-encode-decode-benchmark.js
@@ -0,0 +1,4 @@
+var TestEncodeDecodeBenchmark = require("../browser/test-encode-decode-benchmark.js").TestEncodeDecodeBenchmark;
+
+
+console.log("Data decode usec: " + TestEncodeDecodeBenchmark.benchmarkDataDecodeMicroseconds());  
