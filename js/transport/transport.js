391af775c822bbafcde849224dd37dc5d73e8c81
Jeff Thompson <jefft0@remap.ucla.edu>
2014-06-16 18:08:52 -0700

    transport: Added the Transport base class.

---
+++ js/transport/transport.js
@@ -0,0 +1,35 @@
+/** 
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU General Public License is in the file COPYING.
+ */
+
+/**
+ * Transport is a base class for specific transport classes such as TcpTransport.
+ */
+var Transport = function Transport() 
+{    
+};
+
+exports.Transport = Transport;
+
+/**
+ * Transport.ConnectionInfo is a base class for connection information used by 
+ * subclasses of Transport.
+ */
+Transport.ConnectionInfo = function TransportConnectionInfo() 
+{    
+};
