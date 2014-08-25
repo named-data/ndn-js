bb62848937ea737b6cce114ec16aad0d6dc43f1d
Ryan Bennett <nomad.ry@gmail.com>
2014-08-25 02:03:35 -0600

    modified:   contrib/securityLib/jsbn.js -declare BigInteger via expression vs. declaration (fix undefined object bug)
    NOTE: This file only included when using waf compiled ndn-js.js, ignored via npm

--- contrib/securityLib/jsbn.js
+++ contrib/securityLib/jsbn.js
@@ -24,7 +24,7 @@ var canary = 0xdeadbeefcafe;
 var j_lm = ((canary&0xffffff)==0xefcafe);
 
 // (public) Constructor
-function BigInteger(a,b,c) {
+var BigInteger = function BigInteger(a,b,c) {
   if(a != null)
     if("number" == typeof a) this.fromNumber(a,b,c);
     else if(b == null && "string" != typeof a) this.fromString(a,256);
