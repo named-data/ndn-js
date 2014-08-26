1b495044c9342a59b2fe1ba895f61e8592809e56
Jeff Thompson <jefft0@remap.ucla.edu>
2014-08-26 14:32:42 -0700

    browserify: Added the copyright header to shim files.

--- js/browserify-require.js
+++ js/browserify-require.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Ryan Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU General Public License is in the file COPYING.
+ */
+
 // define a shim require function so that a node/browserify require calls dont cause errors when ndn-js is used via <script> tag
 
 var ndn = ndn || {}
--- js/crypto.js
+++ js/crypto.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2014 Regents of the University of California.
+ * @author: Ryan Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU General Public License is in the file COPYING.
+ */
+
 //wrap a require call to crypto to that we can redirect calls to this file to browserify.js when being built for the browser
 
 module.exports = require('crypto')
