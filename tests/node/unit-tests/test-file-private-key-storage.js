d6af0087914b98f98d419a720a6e308eafd27821
Jeff Thompson <jefft0@remap.ucla.edu>
2015-01-22 09:47:32 -0800

    unit-tests: In test-file-private-key-storage, port the tests from jNDN. Refs #2373.

--- tests/node/unit-tests/test-file-private-key-storage.js
+++ tests/node/unit-tests/test-file-private-key-storage.js
@@ -19,20 +19,97 @@
  */
 
 var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
 var Name = require('../../..').Name;
 var KeyClass = require('../../..').KeyClass;
+var KeyType = require('../../..').KeyType;
+var DigestAlgorithm = require('../../..').DigestAlgorithm;
 var FilePrivateKeyStorage = require('../../..').FilePrivateKeyStorage;
 
-describe('FilePrivateKeyStorage', function () {
-	var storage;
+/**
+ * Retrieve the user's current home directory. (Copy code from file-private-key-storage.js.)
+ * @returns {string} path to the user's home directory
+ */
+function getUserHomePath() {
+	return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
 
-	beforeEach(function () {
-		storage = new FilePrivateKeyStorage();
+var ndnFolder;
+
+describe('FilePrivateKeyStorage', function () {
+  /**
+   * Create a few keys before testing.
+   */
+	before(function () {
+    // (Copy code from file-private-key-storage.js.)
+    ndnFolder = path.join(getUserHomePath(), '.ndn', 'ndnsec-tpm-file');
+    
+    // Create some test key files to use in tests.
+    var instance = new FilePrivateKeyStorage();
+    instance.generateKeyPair(new Name("/test/KEY/123"), KeyType.RSA, 2048);
 	});
 
-	it('should read public key files from the right directory', function () {
-		var key = storage.getPublicKey(new Name('/test/id/ksk-1421778442469'));
-		assert.ok(key.getKeyType() !== null);
-		assert.ok(key.getKeyDer().size() > 0);
+  /**
+   * Delete the keys we created
+   */
+	after(function () {
+    // Delete all keys when done.
+    var instance = new FilePrivateKeyStorage();
+    try {
+      instance.deleteKey(new Name("/test/KEY/123"));
+      instance.deleteKey(new Name("/test/KEY/temp1"));
+    }
+    catch (e){
+      console.log("Failed to clean up generated keys");
+    }
+	});
+
+  /**
+   * Test of generateKeyPair method, of class FilePrivateKeyStorage.
+   */
+	it('GenerateAndDeleteKeys', function () {
+    // Create some more key files.
+    var instance = new FilePrivateKeyStorage();
+    instance.generateKeyPair(new Name("/test/KEY/temp1"), KeyType.RSA, 2048);
+    // Check if the files were created.
+    console.log("ndnFolder " + ndnFolder);
+    var files = fs.readdirSync(ndnFolder);
+    var createdFileCount = files.length;
+    // 2 pre-created + 2 created now + some created by NFD
+    assert.ok(createdFileCount >= 2, "Didn't create 2 files");
+    // Delete these keys.
+    instance.deleteKey(new Name("/test/KEY/temp1"));
+    files = fs.readdirSync(ndnFolder);
+    var deletedfileCount = files.length;
+    assert.ok(createdFileCount - 2 == deletedfileCount, "Didn't delete 2 files");
+	});
+
+  /**
+   * Test of doesKeyExist method, of class FilePrivateKeyStorage.
+   */
+	it('DoesKeyExist', function () {
+    var instance = new FilePrivateKeyStorage();
+    assert.ok(instance.doesKeyExist(new Name("/test/KEY/123"), KeyClass.PRIVATE));
+    assert.ok(!instance.doesKeyExist(new Name("/unknown"), KeyClass.PRIVATE));
+	});
+
+  /**
+   * Test of getPublicKey method, of class FilePrivateKeyStorage.
+   */
+	it('GetPublicKey', function () {
+    var instance = new FilePrivateKeyStorage();
+    var result = instance.getPublicKey(new Name("/test/KEY/123"));
+    assert.notEqual(result, null);
+	});
+
+  /**
+   * Test of sign method, of class FilePrivateKeyStorage.
+   */
+	it('Sign', function () {
+    var data = new Buffer([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+    var instance = new FilePrivateKeyStorage();
+    var result = instance.sign(data, new Name("/test/KEY/123"), DigestAlgorithm.SHA256);
+    assert.notEqual(result, null);
 	});
 });
