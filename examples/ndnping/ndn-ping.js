98dc2201c505d6dd56776f64ceacc54d9771a2f0
Jeff Thompson <jefft0@remap.ucla.edu>
2015-04-10 14:51:37 -0700

    examples/ndnping: Added the standard copyright header.

--- examples/ndnping/ndn-ping.js
+++ examples/ndnping/ndn-ping.js
@@ -1,4 +1,22 @@
 /*
+ * Copyright (C) 2013-2015 Regents of the University of California.
+ * @author: Jeff Burke <jburke@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+/*
   NDN Ping example, revised for ndn-tlv-ping
 
   Using ping responder on NDN testbed, which responds to Interests in
@@ -6,9 +24,6 @@
 
   Jeff Burke
   jburke@remap.ucla.edu
-
-  See COPYING for copyright and distribution information.
-
 */
 
 // One of NDN project default hubs
