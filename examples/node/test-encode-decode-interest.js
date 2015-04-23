ad5e02d3e83ca30e1c61baacb22b2fccc6412ad9
Jeff Thompson <jefft0@remap.ucla.edu>
2015-04-23 11:59:18 -0700

    examples: Remove BinaryXml test data since it is deprecated.

--- examples/node/test-encode-decode-interest.js
+++ examples/node/test-encode-decode-interest.js
@@ -30,8 +30,6 @@ var MemoryPrivateKeyStorage = require('../..').MemoryPrivateKeyStorage;
 var IdentityManager = require('../..').IdentityManager;
 var SelfVerifyPolicyManager = require('../..').SelfVerifyPolicyManager;
 var KeyChain = require('../..').KeyChain;
-var WireFormat = require('../..').WireFormat;
-var TlvWireFormat = require('../..').TlvWireFormat;
 
 var DEFAULT_RSA_PUBLIC_KEY_DER = new Buffer([
     0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01,
@@ -133,25 +131,6 @@ var DEFAULT_RSA_PRIVATE_KEY_DER = new Buffer([
     0x3f, 0xb9, 0xfe, 0xbc, 0x8d, 0xda, 0xcb, 0xea, 0x8f
 ]);
 
-var BinaryXmlInterest = new Buffer([
-0x01, 0xd2,
-  0xf2, 0xfa, 0x9d, 0x6e, 0x64, 0x6e, 0x00, 0xfa, 0x9d, 0x61, 0x62, 0x63, 0x00, 0x00,
-  0x05, 0x9a, 0x9e, 0x31, 0x32, 0x33, 0x00,
-  0x05, 0xa2, 0x8e, 0x34, 0x00,
-  0x03, 0xe2,
-    0x02, 0x85, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
-  0x00,
-  0x02, 0xda, 0xfa, 0x9d, 0x61, 0x62, 0x63, 0x00, 0xea, 0x00, 0x00,
-  0x05, 0xaa, 0x8e, 0x31, 0x00,
-  0x02, 0xfa, 0x8e, 0x34, 0x00,
-  0x02, 0xd2, 0x8e, 0x32, 0x00,
-  0x03, 0x82, 0x9d, 0x01, 0xe0, 0x00, 0x00,
-  0x02, 0xca, 0xb5, 0x61, 0x62, 0x61, 0x62, 0x61, 0x62, 0x00,
-0x00,
-1
-]);
-
 var TlvInterest = new Buffer([
 0x05, 0x53, // Interest
   0x07, 0x0A, 0x08, 0x03, 0x6E, 0x64, 0x6E, 0x08, 0x03, 0x61, 0x62, 0x63, // Name
@@ -212,11 +191,7 @@ function dumpInterest(interest)
 function main()
 {
   var interest = new Interest();
-  // Note: While we transition to the TLV wire format, check if it has been made the default.
-  if (WireFormat.getDefaultWireFormat() == TlvWireFormat.get())
-    interest.wireDecode(new Blob(TlvInterest, false));
-  else
-    interest.wireDecode(new Blob(BinaryXmlInterest, false));
+  interest.wireDecode(new Blob(TlvInterest, false));
   console.log("Interest:");
   dumpInterest(interest);
 
@@ -261,9 +236,7 @@ function main()
   // Make a Face just so that we can sign the interest.
   var face = new Face({host: "localhost"});
   face.setCommandSigningInfo(keyChain, certificateName);
-  if (WireFormat.getDefaultWireFormat() == TlvWireFormat.get())
-    // For the moment, sign and verify of command interests is only supported in TLV.
-    face.makeCommandInterest(freshInterest);
+  face.makeCommandInterest(freshInterest);
 
   var reDecodedFreshInterest = new Interest();
   reDecodedFreshInterest.wireDecode(freshInterest.wireEncode());
@@ -271,12 +244,10 @@ function main()
   console.log("Re-decoded fresh Interest:");
   dumpInterest(reDecodedFreshInterest);
 
-  if (WireFormat.getDefaultWireFormat() == TlvWireFormat.get())
-    // For the moment, sign and verify of command interests is only supported in TLV.
-    keyChain.verifyInterest
-      (reDecodedFreshInterest, 
-       function() { console.log("Freshly-signed Interest signature verification: VERIFIED"); },
-       function() { console.log("Freshly-signed Interest signature verification: FAILED"); });
+  keyChain.verifyInterest
+    (reDecodedFreshInterest, 
+     function() { console.log("Freshly-signed Interest signature verification: VERIFIED"); },
+     function() { console.log("Freshly-signed Interest signature verification: FAILED"); });
 }
 
 main();
