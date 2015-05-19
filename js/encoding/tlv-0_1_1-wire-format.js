e9587df75229cbcb8ba4337b1f92d0d335d34b68
Jeff Thompson <jefft0@remap.ucla.edu>
2015-05-19 09:57:30 -0700

    encoding (minor): Fix the doc for Tlv0_1_1WireFormat.get.

--- js/encoding/tlv-0_1_1-wire-format.js
+++ js/encoding/tlv-0_1_1-wire-format.js
@@ -439,7 +439,7 @@ Tlv0_1_1WireFormat.prototype.encodeSignatureValue = function(signature)
 };
 
 /**
- * Get a singleton instance of a Tlv1_0a2WireFormat.  To always use the
+ * Get a singleton instance of a Tlv0_1_1WireFormat.  To always use the
  * preferred version NDN-TLV, you should use TlvWireFormat.get().
  * @returns {Tlv0_1_1WireFormat} The singleton instance.
  */
