6c57230d22abe21fee44e389e7242bff854d4680
Jeff Thompson <jefft0@remap.ucla.edu>
2015-07-13 15:47:11 -0700

    unit-tests: TestEncryptedContent: Added support for initialVector.

--- tests/node/unit-tests/test-encrypted-content.js
+++ tests/node/unit-tests/test-encrypted-content.js
@@ -29,6 +29,24 @@ var KeyLocatorType = require('../../..').KeyLocatorType;
 var DecodingException = require('../../..').DecodingException;
 
 var ENCRYPTED = new Buffer([
+0x82, 0x30, // EncryptedContent
+  0x1c, 0x16, // KeyLocator
+    0x07, 0x14, // Name
+      0x08, 0x04,
+        0x74, 0x65, 0x73, 0x74, // 'test'
+      0x08, 0x03,
+        0x6b, 0x65, 0x79, // 'key'
+      0x08, 0x07,
+        0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72, // 'locator'
+  0x83, 0x01, // EncryptedAlgorithm
+    0x00,
+  0x85, 0x0a, // InitialVector
+    0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
+  0x84, 0x07, // EncryptedPayload
+    0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
+]);
+
+var ENCRYPTED_NO_IV = new Buffer([
 0x82, 0x24, // EncryptedContent
   0x1c, 0x16, // KeyLocator
     0x07, 0x14, // Name
@@ -48,26 +66,36 @@ var MESSAGE = new Buffer([
   0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
 ]);
 
+var IV = new Buffer([
+  0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73
+]);
+
 describe('TestEncryptedContent', function() {
   it('Constructor', function() {
     var content = new EncryptedContent();
     assert.ok(content.getAlgorithmType() == null);
     assert.ok(content.getPayload().isNull());
+    assert.ok(content.getInitialVector().isNull());
     assert.ok(content.getKeyLocator().getType() == null);
 
     var payload = new Blob(MESSAGE, false);
+    var initialVector = new Blob(IV, false);
+
     var keyLocator = new KeyLocator();
     keyLocator.setType(KeyLocatorType.KEYNAME);
     keyLocator.getKeyName().set("/test/key/locator");
     // TODO: Use AlgorithmSha256WithRsa.
-    content.setAlgorithmType(0).setKeyLocator(keyLocator).setPayload(payload);
+    content.setAlgorithmType(0).setKeyLocator(keyLocator).setPayload(payload)
+      .setInitialVector(initialVector);
 
     // Test the copy constructor.
     var sha256RsaContent = new EncryptedContent(content);
     var contentPayload = sha256RsaContent.getPayload();
+    var contentInitialVector = sha256RsaContent.getInitialVector();
 
     assert.ok(sha256RsaContent.getAlgorithmType() == 0);
     assert.ok(contentPayload.equals(payload));
+    assert.ok(contentInitialVector.equals(initialVector));
     assert.ok(sha256RsaContent.getKeyLocator().getType() != null);
     assert.ok(sha256RsaContent.getKeyLocator().getKeyName().equals
               (new Name("/test/key/locator")));
@@ -80,10 +108,40 @@ describe('TestEncryptedContent', function() {
     sha256RsaContent = new EncryptedContent();
     sha256RsaContent.wireDecode(encryptedBlob);
     contentPayload = sha256RsaContent.getPayload();
+    contentInitialVector = sha256RsaContent.getInitialVector();
 
     // TODO: Use AlgorithmSha256WithRsa.
     assert.ok(sha256RsaContent.getAlgorithmType() == 0);
     assert.ok(contentPayload.equals(payload));
+    assert.ok(contentInitialVector.equals(initialVector));
+    assert.ok(sha256RsaContent.getKeyLocator().getType() != null);
+    assert.ok(sha256RsaContent.getKeyLocator().getKeyName().equals
+              (new Name("/test/key/locator")));
+
+    // Test no IV.
+    sha256RsaContent = new EncryptedContent();
+    sha256RsaContent.setAlgorithmType(0).setKeyLocator(keyLocator).setPayload(payload);
+    contentPayload = sha256RsaContent.getPayload();
+
+    assert.ok(sha256RsaContent.getAlgorithmType() == 0);
+    assert.ok(contentPayload.equals(payload));
+    assert.ok(sha256RsaContent.getInitialVector().isNull());
+    assert.ok(sha256RsaContent.getKeyLocator().getType() != null);
+    assert.ok(sha256RsaContent.getKeyLocator().getKeyName().equals
+              (new Name("/test/key/locator")));
+
+    encryptedBlob = new Blob(ENCRYPTED_NO_IV, false);
+    var encodedNoIv = sha256RsaContent.wireEncode();
+
+    assert.ok(encryptedBlob.equals(encodedNoIv));
+
+    sha256RsaContent = new EncryptedContent();
+    sha256RsaContent.wireDecode(encryptedBlob);
+    contentPayload = sha256RsaContent.getPayload();
+
+    assert.ok(sha256RsaContent.getAlgorithmType() == 0);
+    assert.ok(sha256RsaContent.getPayload().equals(payload));
+    assert.ok(sha256RsaContent.getInitialVector().isNull());
     assert.ok(sha256RsaContent.getKeyLocator().getType() != null);
     assert.ok(sha256RsaContent.getKeyLocator().getKeyName().equals
               (new Name("/test/key/locator")));
@@ -93,7 +151,7 @@ describe('TestEncryptedContent', function() {
     var encryptedContent = new EncryptedContent();
 
     var errorBlob1 = new Blob(new Buffer([
-      0x1f, 0x24, // Wrong EncryptedContent (0x82, 0x24)
+      0x1f, 0x30, // Wrong EncryptedContent (0x82, 0x24)
         0x1c, 0x16, // KeyLocator
           0x07, 0x14, // Name
             0x08, 0x04,
@@ -104,6 +162,8 @@ describe('TestEncryptedContent', function() {
               0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72,
         0x83, 0x01, // EncryptedAlgorithm
           0x00,
+        0x85, 0x0a, // InitialVector
+          0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
         0x84, 0x07, // EncryptedPayload
           0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
     ]), false);
@@ -112,7 +172,7 @@ describe('TestEncryptedContent', function() {
        DecodingException);
 
     var errorBlob2 = new Blob(new Buffer([
-      0x82, 0x24, // EncryptedContent
+      0x82, 0x30, // EncryptedContent
         0x1d, 0x16, // Wrong KeyLocator (0x1c, 0x16)
           0x07, 0x14, // Name
             0x08, 0x04,
@@ -123,6 +183,8 @@ describe('TestEncryptedContent', function() {
               0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72,
         0x83, 0x01, // EncryptedAlgorithm
           0x00,
+        0x85, 0x0a, // InitialVector
+          0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
         0x84, 0x07, // EncryptedPayload
           0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
     ]), false);
@@ -131,7 +193,7 @@ describe('TestEncryptedContent', function() {
        DecodingException);
 
     var errorBlob3 = new Blob(new Buffer([
-      0x82, 0x24, // EncryptedContent
+      0x82, 0x30, // EncryptedContent
         0x1c, 0x16, // KeyLocator
           0x07, 0x14, // Name
             0x08, 0x04,
@@ -142,6 +204,8 @@ describe('TestEncryptedContent', function() {
               0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72,
         0x1d, 0x01, // Wrong EncryptedAlgorithm (0x83, 0x01)
           0x00,
+        0x85, 0x0a, // InitialVector
+          0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
         0x84, 0x07, // EncryptedPayload
           0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
     ]), false);
@@ -150,7 +214,7 @@ describe('TestEncryptedContent', function() {
        DecodingException);
 
     var errorBlob4 = new Blob(new Buffer([
-      0x82, 0x24, // EncryptedContent
+      0x82, 0x30, // EncryptedContent
         0x1c, 0x16, // KeyLocator
           0x07, 0x14, // Name
             0x08, 0x04,
@@ -161,7 +225,9 @@ describe('TestEncryptedContent', function() {
               0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72, // 'locator'
         0x83, 0x01, // EncryptedAlgorithm
           0x00,
-        0x21, 0x07, // EncryptedPayload (0x84, 0x07)
+        0x1f, 0x0a, // InitialVector (0x84, 0x0a)
+          0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
+        0x84, 0x07, // EncryptedPayload
           0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
     ]), false);
     assert.throws
@@ -169,10 +235,31 @@ describe('TestEncryptedContent', function() {
        DecodingException);
 
     var errorBlob5 = new Blob(new Buffer([
-      0x82, 0x00 // Empty EncryptedContent
+      0x82, 0x30, // EncryptedContent
+        0x1c, 0x16, // KeyLocator
+          0x07, 0x14, // Name
+            0x08, 0x04,
+              0x74, 0x65, 0x73, 0x74, // 'test'
+            0x08, 0x03,
+              0x6b, 0x65, 0x79, // 'key'
+            0x08, 0x07,
+              0x6c, 0x6f, 0x63, 0x61, 0x74, 0x6f, 0x72, // 'locator'
+        0x83, 0x01, // EncryptedAlgorithm
+          0x00,
+        0x85, 0x0a, // InitialVector
+          0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x62, 0x69, 0x74, 0x73,
+        0x21, 0x07, // EncryptedPayload (0x85, 0x07)
+          0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74
     ]), false);
     assert.throws
       (function() { encryptedContent.wireDecode(errorBlob5); },
+       DecodingException);
+
+    var errorBlob6 = new Blob(new Buffer([
+      0x82, 0x00 // Empty EncryptedContent
+    ]), false);
+    assert.throws
+      (function() { encryptedContent.wireDecode(errorBlob6); },
        DecodingException);
   });
 
@@ -180,12 +267,14 @@ describe('TestEncryptedContent', function() {
     var content = new EncryptedContent();
     assert.ok(content.getAlgorithmType() == null);
     assert.ok(content.getPayload().isNull());
+    assert.ok(content.getInitialVector().isNull());
     assert.ok(content.getKeyLocator().getType() == null);
 
     // TODO: Use AlgorithmSha256WithRsa.
     content.setAlgorithmType(0);
     assert.ok(content.getAlgorithmType() == 0);
     assert.ok(content.getPayload().isNull());
+    assert.ok(content.getInitialVector().isNull());
     assert.ok(content.getKeyLocator().getType() == null);
 
     var keyLocator = new KeyLocator();
@@ -196,12 +285,19 @@ describe('TestEncryptedContent', function() {
     assert.ok(content.getKeyLocator().getKeyName().equals
               (new Name("/test/key/locator")));
     assert.ok(content.getPayload().isNull());
+    assert.ok(content.getInitialVector().isNull());
 
     var payload = new Blob(MESSAGE, false);
     content.setPayload(payload);
 
     var contentPayload = content.getPayload();
     assert.ok(contentPayload.equals(payload));
+
+    var initialVector = new Blob(IV, false);
+    content.setInitialVector(initialVector);
+
+    var contentInitialVector = content.getInitialVector();
+    assert.ok(contentInitialVector.equals(initialVector));
 
     var encoded = content.wireEncode();
     var contentBlob = new Blob(ENCRYPTED, false);
