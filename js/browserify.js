be5435e17fe987a47af7f9275c9b2af2f8d3eb80
Jeff Thompson <jefft0@remap.ucla.edu>
2016-02-25 11:00:02 -0800

    browserify: Re-add require statements to make it Node-friendly.

--- js/browserify.js
+++ js/browserify.js
@@ -17,6 +17,11 @@
  * A copy of the GNU Lesser General Public License is in the file COPYING.
  */
 
+var ASN1HEX = require('../contrib/securityLib/asn1hex-1.1.js').ASN1HEX
+var KJUR = require('../contrib/securityLib/crypto-1.0.js').KJUR
+var RSAKey = require('../contrib/securityLib/rsasign-1.2.js').RSAKey
+var b64tohex = require('../contrib/securityLib/base64.js').b64tohex
+
 // Library namespace
 var ndn = ndn || {};
 
