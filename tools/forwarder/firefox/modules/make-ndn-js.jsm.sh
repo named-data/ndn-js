6769c758c0a54977127ff151f53565a3babb4e14
Jeff Thompson <jefft0@remap.ucla.edu>
2016-03-17 18:18:03 -0700

    tools/forwarder: In make-ndn-js.jsm.sh, hack to remove Dexie.

--- tools/forwarder/firefox/modules/make-ndn-js.jsm.sh
+++ tools/forwarder/firefox/modules/make-ndn-js.jsm.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-cat ndn-js-header.txt ../../../../build/ndn.min.js ../../../../js/transport/xpcom-transport.js > ndn-js.jsm
+# A hack to remove the last two lines of the file which is Dexie.
+cp ../../../../build/ndn.js /tmp/ndn-no-Dexie.js
+sed -i '' '$d' /tmp/ndn-no-Dexie.js
+sed -i '' '$d' /tmp/ndn-no-Dexie.js
+cat ndn-js-header.txt /tmp/ndn-no-Dexie.js ../../../../js/transport/xpcom-transport.js > ndn-js.jsm
