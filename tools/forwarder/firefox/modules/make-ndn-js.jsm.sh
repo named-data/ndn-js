3d7a1c2fd734e37bd781d031c527b96c7fab38a0
Jeff Thompson <jefft0@remap.ucla.edu>
2016-03-18 12:32:52 -0700

    tools/forwarder (internal): In make-ndn-js.jsm.sh, use a local temp file.

--- tools/forwarder/firefox/modules/make-ndn-js.jsm.sh
+++ tools/forwarder/firefox/modules/make-ndn-js.jsm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # A hack to remove the last two lines of the file which is Dexie.
-cp ../../../../build/ndn.js /tmp/ndn-no-Dexie.js
-sed -i '' '$d' /tmp/ndn-no-Dexie.js
-sed -i '' '$d' /tmp/ndn-no-Dexie.js
-cat ndn-js-header.txt /tmp/ndn-no-Dexie.js ../../../../js/transport/xpcom-transport.js > ndn-js.jsm
+cp ../../../../build/ndn.js ndn-no-Dexie.js
+sed -i '' '$d' ndn-no-Dexie.js
+sed -i '' '$d' ndn-no-Dexie.js
+cat ndn-js-header.txt ndn-no-Dexie.js ../../../../js/transport/xpcom-transport.js > ndn-js.jsm
+rm ndn-no-Dexie.js
