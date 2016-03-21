86e7c15528fca0736d366caa7972243b0ccb7467
Jeff Thompson <jefft0@remap.ucla.edu>
2016-03-21 09:10:24 -0700

    tools: Removed forwarder. (Now using micro-forwarder.)

--- tools/forwarder/firefox/modules/make-ndn-js.jsm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# A hack to remove the last two lines of the file which is Dexie.
-cp ../../../../build/ndn.js ndn-no-Dexie.js
-sed -i '' '$d' ndn-no-Dexie.js
-sed -i '' '$d' ndn-no-Dexie.js
-cat ndn-js-header.txt ndn-no-Dexie.js ../../../../js/transport/xpcom-transport.js > ndn-js.jsm
-rm ndn-no-Dexie.js
