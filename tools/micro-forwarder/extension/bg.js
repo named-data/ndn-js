6281a239407a6cb632fa063d3b297714f2891bb2
Jeff Thompson <jefft0@remap.ucla.edu>
2016-03-21 10:54:15 -0700

    tools/micro-forwarder (internal): In bg.js, use console.log instead of dump.

--- tools/micro-forwarder/extension/bg.js
+++ tools/micro-forwarder/extension/bg.js
@@ -45,7 +45,7 @@ var ForwarderFace = function ForwarderFace(port)
  */
 ForwarderFace.prototype.onReceivedElement = function(element)
 {
-  if (LOG > 3) dump("Complete element received. Length " + element.length + "\n");
+  if (LOG > 3) console.log("Complete element received. Length " + element.length + "\n");
   // First, decode as Interest or Data.
   var interest = null;
   var data = null;
@@ -63,7 +63,7 @@ ForwarderFace.prototype.onReceivedElement = function(element)
 
   // Now process as Interest or Data.
   if (interest !== null) {
-    if (LOG > 3) dump("Interest packet received: " + interest.getName().toUri() + "\n");
+    if (LOG > 3) console.log("Interest packet received: " + interest.getName().toUri() + "\n");
     if (ForwarderFace.localhostNamePrefix.match(interest.getName())) {
       this.onReceivedLocalhostInterest(interest);
       return;
@@ -83,7 +83,7 @@ ForwarderFace.prototype.onReceivedElement = function(element)
     PIT.push(pitEntry);
     // Set the interest timeout timer.
     var timeoutCallback = function() {
-      if (LOG > 3) dump("Interest time out: " + interest.getName().toUri() + "\n");
+      if (LOG > 3) console.log("Interest time out: " + interest.getName().toUri() + "\n");
       // Remove this entry from the PIT
       var index = PIT.indexOf(pitEntry);
       if (index >= 0)
@@ -113,13 +113,13 @@ ForwarderFace.prototype.onReceivedElement = function(element)
     }
   }
   else if (data !== null) {
-    if (LOG > 3) dump("Data packet received: " + data.getName().toUri() + "\n");
+    if (LOG > 3) console.log("Data packet received: " + data.getName().toUri() + "\n");
 
     // Send the data packet to the face for each matching PIT entry.
     // Iterate backwards so we can remove the entry and keep iterating.
     for (var i = PIT.length - 1; i >= 0; --i) {
       if (PIT[i].interest.matchesName(data.getName())) {
-        if (LOG > 3) dump("Sending Data to match interest " + PIT[i].interest.getName().toUri() + "\n");
+        if (LOG > 3) console.log("Sending Data to match interest " + PIT[i].interest.getName().toUri() + "\n");
         PIT[i].face.port.postMessage(element);
 
         // Remove this entry.
@@ -141,12 +141,12 @@ ForwarderFace.prototype.onReceivedLocalhostInterest = function(interest)
     try {
       controlParameters.wireDecode(interest.getName().get(4).getValue());
     } catch (ex) {
-      if (LOG > 3) dump("Error decoding register interest ControlParameters " + ex + "\n");
+      if (LOG > 3) console.log("Error decoding register interest ControlParameters " + ex + "\n");
       return;
     }
     // TODO: Verify the signature?
 
-    if (LOG > 3) dump("Received register request " + controlParameters.getName().toUri() + "\n");
+    if (LOG > 3) console.log("Received register request " + controlParameters.getName().toUri() + "\n");
     this.registeredPrefixes.push(controlParameters.getName());
 
     // Send the ControlResponse.
@@ -160,7 +160,7 @@ ForwarderFace.prototype.onReceivedLocalhostInterest = function(interest)
     this.port.postMessage(responseData.wireEncode().buf());
   }
   else {
-    if (LOG > 3) dump("Unrecognized localhost prefix " + interest.getName() + "\n");
+    if (LOG > 3) console.log("Unrecognized localhost prefix " + interest.getName() + "\n");
   }
 };
 
