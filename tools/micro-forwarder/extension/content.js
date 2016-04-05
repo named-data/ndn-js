7d68cd400459eb1e26e2757215e9bd35c827c0f8
Wentao Shang <wentaoshang@gmail.com>
2016-04-05 13:47:50 -0700

    tools/micro-forwarder: initialize port on-demand in content script

--- tools/micro-forwarder/extension/content.js
+++ tools/micro-forwarder/extension/content.js
@@ -1,21 +1,25 @@
-var port = chrome.runtime.connect();
-// Add a listener to wait for msg from background script
-port.onMessage.addListener(function(msg) {
-	window.postMessage({
-		type: "FromMicroForwarderStub",
-		object: msg
-	}, "*");
-});
+var port = null;
 
 // Add listener to wait for msg from app scripts
 window.addEventListener("message", function(event) {
-	// We only accept messages from ourselves
-	if (event.source != window)
-	  return;
+    // We only accept messages from ourselves
+    if (event.source != window)
+	return;
 	
-	if (event.data.type && (event.data.type == "FromMicroForwarderTransport")) {
-	  port.postMessage(event.data.object);
+    if (event.data.type && (event.data.type == "FromMicroForwarderTransport")) {
+	if (port == null) {
+	    port = chrome.runtime.connect();
+	    // Add a listener to wait for msg from background script
+	    port.onMessage.addListener(function(msg) {
+		window.postMessage({
+		    type: "FromMicroForwarderStub",
+		    object: msg
+		}, "*");
+	    });
+	    console.log("Forwarder stub connected to background script.");
 	}
+	port.postMessage(event.data.object);
+    }
 }, false);
 
 console.log("NDN Micro Forwarder stub is loaded.");
