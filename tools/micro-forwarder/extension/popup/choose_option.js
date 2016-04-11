9246088e241aedcc068d36c4cc649eae086d2163
Wentao Shang <wentaoshang@gmail.com>
2016-04-11 16:24:26 -0700

    tools/micro-fowarder: fix issue that prevents popup button to open config page in Chrome

--- tools/micro-forwarder/extension/popup/choose_option.js
+++ tools/micro-forwarder/extension/popup/choose_option.js
@@ -1,7 +1,12 @@
 function showConfig() {
-  chrome.tabs.create(
-    {
-      url: chrome.extension.getURL("config.html")
-    }
-  );
+    chrome.tabs.create({
+	url: chrome.extension.getURL("config.html")
+    });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var btn = document.getElementById("showConfig");
+    btn.addEventListener("click", function() {
+	showConfig();
+    });
+});
