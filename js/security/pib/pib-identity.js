782a81e941da4d6e00f70bcf0f7b542e20844319
Jeff Thompson <jefft0@remap.ucla.edu>
2017-11-01 04:07:33 -0700

    security: Added PibIdentity and PibKey. Refs #4045.

---
+++ js/security/pib/pib-identity.js
@@ -0,0 +1,167 @@
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/pib/identity.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+/** @ignore */
+var SyncPromise = require('../../util/sync-promise.js').SyncPromise;
+
+/**
+ * A PibIdentity is at the top level in PIB's Identity-Key-Certificate hierarchy.
+ * An identity has a Name, and contains zero or more keys, at most one of which
+ * is set as the default key of this identity. Properties of a key can be
+ * accessed after obtaining a Key object.
+ *
+ * Create a PibIdentity which uses the impl backend implementation. This
+ * constructor should only be called by PibIdentityContainer.
+ *
+ * @param {PibIdentityImpl} impl The PibIdentityImpl.
+ * @constructor
+ */
+var PibIdentity = function PibIdentity(impl)
+{
+  this.impl_ = impl;
+};
+
+exports.PibIdentity = PibIdentity;
+
+/**
+ * Get the name of the identity.
+ * @return {Name} The name of the identity. You must not change the Name object.
+ * If you need to change it then make a copy.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibIdentity.prototype.getName = function()
+{
+  return this.lock_().getName();
+};
+
+/**
+ * Get the key with name keyName.
+ * @param {Name} keyName The name of the key.
+ * @return {Promise|SyncPromise} A promise which returns the PibKey object, or a
+ * promise rejected with Pib.Error if the key does not exist, or a promise
+ * rejected with Error if the backend implementation instance is invalid.
+ */
+PibIdentity.prototype.getKeyPromise = function(keyName)
+{
+  try {
+    return this.lock_().getKeyPromise(keyName);
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Get the default key of this Identity.
+ * @return {Promise|SyncPromise} A promise which returns the PibKey object of
+ * the default key, or a promise rejected with Pib.Error if the default key has
+ * not been set, or a promise rejected with Error if the backend implementation
+ * instance is invalid.
+ */
+PibIdentity.prototype.getDefaultKeyPromise = function()
+{
+  try {
+    return this.lock_().getDefaultKeyPromise();
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Add the key. If a key with the same name already exists, overwrite the key. 
+ * If no default key for the identity has been set, then set the added key as 
+ * default for the identity. This should only be called by KeyChain.
+ * @param {Buffer} key The public key bits. This copies the buffer.
+ * @param {Name} keyName The name of the key. This copies the name.
+ * @return {Promise|SyncPromise} A promise which returns the PibKey object, or a
+ * promise rejected with Error if the backend implementation instance is invalid.
+ */
+PibIdentity.prototype.addKeyPromise_ = function(key, keyName)
+{
+  try {
+    return this.lock_().addKeyPromise(key, keyName);
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Remove the key with keyName and its related certificates. If the key does not
+ * exist, do nothing. This should only be called by KeyChain.
+ * @param {Name} keyName The name of the key.
+ * @return {Promise|SyncPromise} A promise which fulfills when finished, or a
+ * promise rejected with Error if the backend implementation instance is invalid.
+ */
+PibIdentity.prototype.removeKeyPromise_ = function(keyName)
+{
+  try {
+    return this.lock_().removeKeyPromise(keyName);
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * setDefaultKey has two forms:
+ * setDefaultKey(keyName) - Set the key with name keyName as the default key of
+ * the identity.
+ * setDefaultKey(key, keyName) - Add a key with name keyName and set it as the
+ * default key of the identity.
+ * This should only be called by KeyChain.
+ * @param {Buffer} key The buffer of encoded key bytes. (This is only used when
+ * calling setDefaultKey(key, keyName). )
+ * @param {Name} keyName The name of the key. This copies the name.
+ * @return {SyncPromise} A promise which returns the PibKey object of the
+ * default key, or a promise rejected with Error the name of the key does not
+ * match the identity name (or if the backend implementation instance is 
+ * invalid), or a promise rejected with Pib.Error if calling
+ * setDefaultKey(keyName) and the key does not exist, or if calling
+ * setDefaultKey(key, keyName) and a key with the same name already exists.
+ */
+PibIdentity.prototype.setDefaultKeyPromise_ = function(keyOrKeyName, arg2)
+{
+  try {
+    return this.lock_().setDefaultKeyPromise(keyOrKeyName, arg2);
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Get the PibKeyContainer in the PibIdentityImpl. This should only be called by
+ * KeyChain.
+ * @return {PibKeyContainer} The PibKeyContainer.
+ */
+PibIdentity.prototype.getKeys_ = function()
+{
+  return this.lock_().keys_;
+};
+
+/**
+ * Check the validity of the impl_ instance.
+ * @return {PibIdentityImpl} The PibIdentityImpl when the instance is valid.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibIdentity.prototype.lock_ = function()
+{
+  if (this.impl_ == null)
+    throw new Error("Invalid key instance");
+
+  return this.impl_;
+};
---
+++ js/security/pib/pib-key.js
@@ -0,0 +1,218 @@
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/pib/key.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+/** @ignore */
+var Name = require('../../name.js').Name; /** @ignore */
+var CertificateV2 = require('../v2/certificate-v2.js').CertificateV2; /** @ignore */
+var SyncPromise = require('../../util/sync-promise.js').SyncPromise;
+
+/**
+ * A PibKey provides access to a key at the second level in the PIB's 
+ * Identity-Key-Certificate hierarchy. A PibKey object has a Name (identity +
+ * "KEY" + keyId), and contains one or more CertificateV2 objects, one of which
+ * is set as the default certificate of this key. A certificate can be directly
+ * accessed by getting a CertificateV2 object.
+ *
+ * Create a PibKey which uses the impl backend implementation. This constructor
+ * should only be called by PibKeyContainer.
+ *
+ * @param {PibKeyImpl} impl An object of a subclass of PibKeyImpl.
+ * @constructor
+ */
+var PibKey = function PibKey(impl)
+{
+  this.impl_ = impl;
+};
+
+exports.PibKey = PibKey;
+
+/**
+ * Get the key name.
+ * @return {Name} The key name. You must not modify the Key object. If you need
+ * to modify it, make a copy.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibKey.prototype.getName = function() { return this.lock_().getName(); };
+
+/**
+ * Get the name of the identity this key belongs to.
+ * @return {Name} The name of the identity. You must not modify the Key object.
+ * If you need to modify it, make a copy.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibKey.prototype.getIdentityName = function()
+{
+  return this.lock_().getIdentityName();
+};
+
+/**
+ * Get the key type.
+ * @return {number} The key type as an int from the KeyType enum.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibKey.prototype.getKeyType = function() { return this.lock_().getKeyType(); };
+
+/**
+ * Get the public key encoding.
+ * @return {Blob} The public key encoding.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibKey.prototype.getPublicKey = function() { return this.lock_().getPublicKey(); };
+
+/**
+ * Get the certificate with name certificateName.
+ * @param {Name} certificateName The name of the certificate.
+ * @return {Promise|SyncPromise} A promise which returns a copy of the
+ * CertificateV2, or a promise rejected with Error if certificateName does not
+ * match the key name (or if the backend implementation instance is invalid), or
+ * a promise rejected with Pib.Error if the certificate does not exist.
+ */
+PibKey.prototype.getCertificatePromise = function(certificateName)
+{
+  try {
+    return this.lock_().getCertificatePromise(certificateName);
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Get the default certificate for this Key.
+ * @return {Promise|SyncPromise} A promise which returns the default
+ * CertificateV2, or a promise rejected with Error if the backend implementation
+ * instance is invalid, or a promise rejected with Pib.Error if the default
+ * certificate does not exist.
+ */
+PibKey.prototype.getDefaultCertificatePromise = function()
+{
+  try {
+    return this.lock_().getDefaultCertificatePromise();
+  } catch (ex) {
+    return SyncPromise.reject(ex);
+  }
+};
+
+/**
+ * Construct a key name based on the appropriate naming conventions.
+ * @param {Name} identityName The name of the identity.
+ * @param {Name.Component} keyId The key ID name component.
+ * @return {Name} The constructed name as a new Name.
+ */
+PibKey.constructKeyName = function(identityName, keyId)
+{
+  var keyName = new Name(identityName);
+  keyName.append(CertificateV2.KEY_COMPONENT).append(keyId);
+
+  return keyName;
+};
+
+/**
+ * Check if keyName follows the naming conventions for a key name.
+ * @param {Name} keyName The name of the key.
+ * @return {boolean} True if keyName follows the naming conventions, otherwise
+ * false.
+ */
+PibKey.isValidKeyName = function(keyName)
+{
+  return keyName.size() > CertificateV2.MIN_KEY_NAME_LENGTH &&
+         keyName.get(-CertificateV2.MIN_KEY_NAME_LENGTH).equals
+           (CertificateV2.KEY_COMPONENT);
+};
+
+/**
+ * Extract the identity namespace from keyName.
+ * @param {Name} keyName The name of the key.
+ * @return {Name} The identity name as a new Name.
+ */
+PibKey.extractIdentityFromKeyName = function(keyName)
+{
+  if (!PibKey.isValidKeyName(keyName))
+    throw new Error("Key name `" + keyName.toUri() +
+      "` does not follow the naming conventions");
+
+  // Trim everything after and including "KEY".
+  return keyName.getPrefix(-CertificateV2.MIN_KEY_NAME_LENGTH);
+};
+
+/**
+ * Add the certificate. If a certificate with the same name (without implicit
+ * digest) already exists, then overwrite the certificate. If no default
+ * certificate for the key has been set, then set the added certificate as
+ * default for the key. This should only be called by KeyChain.
+ * @param {CertificateV2} certificate The certificate to add. This copies the
+ * object.
+ * @return {Promise|SyncPromise} A promise which fulfills when finished, or a
+ * promise rejected with Error if the name of the certificate does not match the
+ * key name.
+ */
+PibKey.prototype.addCertificatePromise_ = function(certificate)
+{
+  return this.lock_().addCertificatePromise(certificate);
+};
+
+/**
+ * Remove the certificate with name certificateName. If the certificate does not
+ * exist, do nothing. This should only be called by KeyChain.
+ * @param {Name} certificateName The name of the certificate.
+ * @return {Promise|SyncPromise} A promise which fulfills when finished, or a
+ * promise rejected with Error if certificateName does not match the key name.
+ */
+PibKey.prototype.removeCertificatePromise_ = function(certificateName)
+{
+  return this.lock_().removeCertificatePromise(certificateName);
+};
+
+/**
+ * Set the existing certificate with name certificateName as the default
+ * certificate. This should only be called by KeyChain.
+ * @param {Name} certificateName The name of the certificate.
+ * @return {Promise|SyncPromise} A promise which returns the default
+ * CertificateV2, or a promise rejected with Error if certificateName does not
+ * match the key name, or a promise rejected with Pib.Error if
+ * certificateOrCertificateName is the certificate Name and the certificate does
+ * not exist.
+ */
+PibKey.prototype.setDefaultCertificatePromise_ = function(certificateName)
+{
+  return this.lock_().setDefaultCertificatePromise(certificateName);
+};
+
+/**
+ * Get the PibCertificateContainer in the PibKeyImpl. This should only be called
+ * by KeyChain.
+ * @return {PibCertificateContainer} The PibCertificateContainer.
+ */
+PibKey.prototype.getCertificates_ = function()
+{
+  return this.lock_().certificates_;
+};
+
+/**
+ * Check the validity of the impl_ instance.
+ * @return {PibKeyImpl} The PibKeyImpl when the instance is valid.
+ * @throws Error if the backend implementation instance is invalid.
+ */
+PibKey.prototype.lock_ = function()
+{
+  if (this.impl_ == null)
+    throw new Error("Invalid key instance");
+
+  return this.impl_;
+};
