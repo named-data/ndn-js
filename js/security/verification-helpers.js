9823d872be9c147eb3a61016807be14c809e54dd
Jeff Thompson <jefft0@remap.ucla.edu>
2017-11-09 13:58:56 -0800

    security: In VerificationHelpers, added sync verifySignature.

--- js/security/verification-helpers.js
+++ js/security/verification-helpers.js
@@ -143,6 +143,51 @@ VerificationHelpers.verifySignaturePromise = function
 };
 
 /**
+ * Verify the buffer against the signature using the public key.
+ * @param {Buffer|Blob} buffer The input buffer to verify.
+ * @param {Buffer|Blob} signature The signature bytes.
+ * @param {PublicKey|Buffer:Blob} publicKey The object containing the public key,
+ * or the public key DER which is used to make the PublicKey object.
+ * @param {number} digestAlgorithm (optional) The digest algorithm as an int
+ * from the DigestAlgorithm enum. If omitted, use DigestAlgorithm.SHA256.
+ * @param {function} onComplete (optional) This calls
+ * onComplete(result) with true if verification succeeds, false if verification
+ * fails. If omitted, the return value is described below. (Some crypto
+ * libraries only use a callback, so onComplete is required to use these.)
+ * NOTE: The library will log any exceptions thrown by this callback, but for
+ * better error handling the callback should catch and properly handle any
+ * exceptions.
+ * @param {function} onError (optional) If defined, then onComplete must be
+ * defined and if there is an exception, then this calls onError(exception)
+ * with the exception. If onComplete is defined but onError is undefined, then
+ * this will log any thrown exception. (Some crypto libraries only use a
+ * callback, so onError is required to be notified of an exception.)
+ * NOTE: The library will log any exceptions thrown by this callback, but for
+ * better error handling the callback should catch and properly handle any
+ * exceptions.
+ * @return {boolean} If onComplete is omitted, return true if verification
+ * succeeds, false if verification fails. Otherwise, if onComplete is supplied
+ * then return undefined and use onComplete as described above.
+ * @throws Error for an invalid public key type or digestAlgorithm. However, if
+ * onComplete and onError are defined, then if there is an exception return
+ * undefined and call onError(exception).
+ */
+VerificationHelpers.verifySignature = function
+  (buffer, signature, publicKey, digestAlgorithm, onComplete, onError)
+{
+  if (typeof digestAlgorithm === 'boolean') {
+    // digestAlgorithm is omitted, so shift.
+    onError = onComplete;
+    onComplete = digestAlgorithm;
+    digestAlgorithm = undefined;
+  }
+
+  return SyncPromise.complete(onComplete, onError,
+    this.verifySignaturePromise
+      (buffer, signature, publicKey, digestAlgorithm, !onComplete));
+};
+
+/**
  * Verify the buffer against the digest using the digest algorithm.
  * @param {Buffer|Blob} buffer The input buffer to verify.
  * @param {Buffer|Blob} digest The digest bytes.
