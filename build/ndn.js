eddb5b6e66d9a909b22b1d22e58adea6395a865d
Jeff Thompson <jefft0@remap.ucla.edu>
2018-03-26 16:05:25 -0700

    ndn.js: Rebuild to get CommandInterestSigner and ValidationPolicyCommandInterest.

--- build/ndn.js
+++ build/ndn.js
@@ -18936,6 +18936,176 @@ exports.DigestAlgorithm = DigestAlgorithm;
 
 DigestAlgorithm.SHA256 = 1;
 /**
+ * Copyright (C) 2017-2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/command-interest-signer.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+// Use capitalized Crypto to not clash with the browser's crypto.subtle.
+/** @ignore */
+var Crypto = require('../crypto.js'); /** @ignore */
+var Interest = require('../interest.js').Interest; /** @ignore */
+var WireFormat = require('../encoding/wire-format.js').WireFormat; /** @ignore */
+var TlvEncoder = require('../encoding/tlv/tlv-encoder.js').TlvEncoder; /** @ignore */
+var Blob = require('../util/blob.js').Blob; /** @ignore */
+var SigningInfo = require('./signing-info.js').SigningInfo;
+
+/**
+ * CommandInterestSigner is a helper class to create command interests. This
+ * keeps track of a timestamp and generates command interests by adding name
+ * components according to the NFD Signed Command Interests protocol.
+ * See makeCommandInterest() for details.
+ * https://redmine.named-data.net/projects/ndn-cxx/wiki/CommandInterest
+ *
+ * Create a CommandInterestSigner to use the keyChain to sign.
+ * @param {KeyChain} keyChain The KeyChain used to sign.
+ * @constructor
+ */
+var CommandInterestSigner = function CommandInterestSigner(keyChain)
+{
+  this.keyChain_ = keyChain;
+  this.lastUsedTimestamp = Math.round(new Date().getTime());
+  this.nowOffsetMilliseconds_ = 0;
+};
+
+exports.CommandInterestSigner = CommandInterestSigner;
+
+CommandInterestSigner.POS_SIGNATURE_VALUE = -1;
+CommandInterestSigner.POS_SIGNATURE_INFO =  -2;
+CommandInterestSigner.POS_NONCE =           -3;
+CommandInterestSigner.POS_TIMESTAMP =       -4;
+
+CommandInterestSigner.MINIMUM_SIZE = 4;
+
+/**
+ * Append the timestamp and nonce name components to the supplied name, create
+ * an Interest object and signs it with the KeyChain given to the constructor.
+ * This ensures that the timestamp is greater than the timestamp used in the
+ * previous call.
+ * @param {Name} name The Name for the Interest, which is copied.
+ * @param {SigningInfo} params (optional) The signing parameters. If omitted,
+ * use a default SigningInfo().
+ * @param {WireFormat} wireFormat (optional) A WireFormat object used to encode
+ * the SignatureInfo and to encode interest name for signing. If omitted, use
+ * WireFormat getDefaultWireFormat().
+ * @param {function} onComplete (optional) This calls onComplete(interest) with
+ * the new command Interest object. (Some crypto libraries only use a callback,
+ * so onComplete is required to use these.)
+ * NOTE: The library will log any exceptions thrown by this callback, but for
+ * better error handling the callback should catch and properly handle any
+ * exceptions.
+ * @param {function} onError (optional) If defined, then onComplete must be
+ * defined and if there is an exception, then this calls onError(exception)
+ * with the exception. If onComplete is defined but onError is undefined, then
+ * this will log any thrown exception. (Some database libraries only use a
+ * callback, so onError is required to be notified of an exception.)
+ * NOTE: The library will log any exceptions thrown by this callback, but for
+ * better error handling the callback should catch and properly handle any
+ * exceptions.
+ * @return {Interest} If onComplete is omitted, return the new command Interest
+ * object. Otherwise, if onComplete is supplied then return undefined and use
+ * onComplete as described above.
+ */
+CommandInterestSigner.prototype.makeCommandInterest = function
+  (name, params, wireFormat, onComplete, onError)
+{
+  var arg2 = params;
+  var arg3 = wireFormat;
+  var arg4 = onComplete;
+  var arg5 = onError;
+  // arg2,       arg3,       arg4,       arg5
+  // params,     wireFormat, onComplete, onError
+  // params,     wireFormat, null,       null
+  // params,     onComplete, onError,    null
+  // params,     null,       null,       null
+  // wireFormat, onComplete, onError,    null
+  // wireFormat, null,       null,       null
+  // onComplete, onError,    null,       null
+  // null,       null,       null,       null
+  if (arg2 instanceof SigningInfo)
+    params = arg2;
+  else
+    params = undefined;
+
+  if (arg2 instanceof WireFormat)
+    wireFormat = arg2;
+  else if (arg3 instanceof WireFormat)
+    wireFormat = arg3;
+  else
+    wireFormat = undefined;
+
+  if (typeof arg2 === "function") {
+    onComplete = arg2;
+    onError = arg3;
+  }
+  else if (typeof arg3 === "function") {
+    onComplete = arg3;
+    onError = arg4;
+  }
+  else if (typeof arg4 === "function") {
+    onComplete = arg4;
+    onError = arg5;
+  }
+  else {
+    onComplete = undefined;
+    onError = undefined;
+  }
+
+  if (params == undefined)
+    params = new SigningInfo();
+
+  if (wireFormat == undefined)
+    wireFormat = WireFormat.getDefaultWireFormat();
+
+  // This copies the Name.
+  var commandInterest = new Interest(name);
+
+  var now = new Date().getTime() + this.nowOffsetMilliseconds_;
+  var timestamp = Math.round(now);
+  while (timestamp <= this.lastTimestamp)
+    timestamp += 1.0;
+
+  // The timestamp is encoded as a TLV nonNegativeInteger.
+  var encoder = new TlvEncoder(8);
+  encoder.writeNonNegativeInteger(timestamp);
+  commandInterest.getName().append(new Blob(encoder.getOutput(), false));
+
+  // The random value is a TLV nonNegativeInteger too, but we know it is 8
+  // bytes, so we don't need to call the nonNegativeInteger encoder.
+  commandInterest.getName().append(new Blob(Crypto.randomBytes(8), false));
+
+  // Update the timestamp before calling async sign.
+  this.lastTimestamp = timestamp;
+
+  return this.keyChain_.sign
+    (commandInterest, params, wireFormat, onComplete, onError);
+};
+
+/**
+ * Set the offset for when makeCommandInterest() gets the current time, which
+ * should only be used for testing.
+ * @param {number} nowOffsetMilliseconds The offset in milliseconds.
+ */
+CommandInterestSigner.prototype.setNowOffsetMilliseconds_ = function
+  (nowOffsetMilliseconds)
+{
+  this.nowOffsetMilliseconds_ = nowOffsetMilliseconds;
+};
+/**
  * Copyright (C) 2014-2018 Regents of the University of California.
  * @author: Jeff Thompson <jefft0@remap.ucla.edu>
  * From ndn-cxx security by Yingdi Yu <yingdi@cs.ucla.edu>.
@@ -36431,6 +36601,317 @@ ValidationPolicyAcceptAll.prototype.checkPolicy = function
   (dataOrInterest, state, continueValidation)
 {
   continueValidation(null, state);
+};
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/v2/validation-policy-command-interest.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+/** @ignore */
+var Name = require('../../name.js').Name; /** @ignore */
+var Data = require('../../data.js').Data; /** @ignore */
+var CommandInterestSigner = require('../command-interest-signer.js').CommandInterestSigner; /** @ignore */
+var ValidationError = require('./validation-error.js').ValidationError; /** @ignore */
+var ValidationPolicy = require('./validation-policy.js').ValidationPolicy;
+
+/**
+ * ValidationPolicyCommandInterest extends ValidationPolicy as a policy for
+ * stop-and-wait command Interests. See:
+ * https://redmine.named-data.net/projects/ndn-cxx/wiki/CommandInterest
+ *
+ * This policy checks the timestamp field of a stop-and-wait command Interest.
+ * Signed Interest validation and Data validation requests are delegated to an
+ * inner policy.
+ *
+ * Create a ValidationPolicyCommandInterest.
+ * @param innerPolicy {ValidationPolicy} a ValidationPolicy for signed Interest
+ * signature validation and Data validation. This must not be null.
+ * @param {ValidationPolicyCommandInterest.Options} options (optional) The
+ * stop-and-wait command Interest validation options.
+ * @throws Error if innerPolicy is null.
+ * @constructor
+ */
+var ValidationPolicyCommandInterest = function ValidationPolicyCommandInterest
+  (innerPolicy, options)
+{
+  // Call the base constructor.
+  ValidationPolicy.call(this);
+
+  if (options == undefined)
+    this.options_ = new ValidationPolicyCommandInterest.Options();
+  else
+    // Copy the Options.
+    this.options_ = new ValidationPolicyCommandInterest.Options(options);
+
+  this.container_ = []; // of ValidationPolicyCommandInterest.LastTimestampRecord
+  this.nowOffsetMilliseconds_ = 0;
+
+  if (innerPolicy == null)
+    throw new Error("inner policy is missing");
+
+  this.setInnerPolicy(innerPolicy);
+
+  if (this.options_.gracePeriod_ < 0.0)
+    this.options_.gracePeriod_ = 0.0;
+};
+
+ValidationPolicyCommandInterest.prototype = new ValidationPolicy();
+ValidationPolicyCommandInterest.prototype.name = "ValidationPolicyCommandInterest";
+
+exports.ValidationPolicyCommandInterest = ValidationPolicyCommandInterest;
+
+/**
+ * Create a ValidationPolicyCommandInterest.Options with the values.
+ * @param {number|ValidationPolicyCommandInterest.Options} gracePeriodOrOptions
+ * (optional) The tolerance of the initial timestamp in milliseconds. (However,
+ * if this is another ValidationPolicyCommandInterest.Options, then copy values
+ * from it.) If omitted, use a grace period of 2 minutes. A stop-and-wait
+ * command Interest is considered "initial" if the validator has not recorded
+ * the last timestamp from the same public key, or when such knowledge has been
+ * erased. For an initial command Interest, its timestamp is compared to the
+ * current system clock, and the command Interest is rejected if the absolute
+ * difference is greater than the grace interval. The grace period should be
+ * positive. Setting this option to 0 or negative causes the validator to
+ * require exactly the same timestamp as the system clock, which most likely
+ * rejects all command Interests.
+ * @param {number} maxRecords (optional) The maximum number of distinct public
+ * keys of which to record the last timestamp. If omitted, use 1000. The
+ * validator records the last timestamps for every public key. For a subsequent
+ * command Interest using the same public key, its timestamp is compared to the
+ * last timestamp from that public key, and the command Interest is rejected if
+ * its timestamp is less than or equal to the recorded timestamp.
+ * This option limits the number of distinct public keys being tracked. If the
+ * limit is exceeded, then the oldest record is deleted.
+ * Setting max records to -1 allows tracking unlimited public keys. Setting max
+ * records to 0 disables using last timestamp records and causes every command
+ * Interest to be processed as initial.
+ * @param {number} recordLifetime (optional) The maximum lifetime of a last
+ * timestamp record in milliseconds. If omitted, use 1 hour. A last timestamp
+ * record expires and can be deleted if it has not been refreshed within the
+ * record lifetime. Setting the record lifetime to 0 or negative makes last
+ * timestamp records expire immediately and causes every command Interest to be
+ * processed as initial.
+ * @constructor
+ */
+ValidationPolicyCommandInterest.Options = function ValidationPolicyCommandInterestOptions
+  (gracePeriodOrOptions, maxRecords, recordLifetime)
+{
+  if (gracePeriodOrOptions instanceof ValidationPolicyCommandInterest.Options) {
+    // The copy constructor.
+    var options = gracePeriodOrOptions;
+
+    this.gracePeriod_ = options.gracePeriod_;
+    this.maxRecords_ = options.maxRecords_;
+    this.recordLifetime_ = options.recordLifetime_;
+  }
+  else {
+    var gracePeriod = gracePeriodOrOptions;
+
+    if (gracePeriod == undefined)
+      gracePeriod = 2 * 60 * 1000.0;
+    if (maxRecords == undefined)
+      maxRecords = 1000;
+    if (recordLifetime == undefined)
+      recordLifetime = 3600 * 1000.0;
+
+    this.gracePeriod_ = gracePeriod;
+    this.maxRecords_ = maxRecords;
+    this.recordLifetime_ = recordLifetime;
+  }
+};
+
+/**
+ * @param {Data|Interest} dataOrInterest
+ * @param {ValidationState} state
+ * @param {function} continueValidation
+ */
+ValidationPolicyCommandInterest.prototype.checkPolicy = function
+  (dataOrInterest, state, continueValidation)
+{
+  if (dataOrInterest instanceof Data) {
+    var data = dataOrInterest;
+    this.getInnerPolicy().checkPolicy(data, state, continueValidation);
+  }
+  else {
+    var interest = dataOrInterest;
+
+    var keyName = [null];
+    var timestamp = [0];
+    if (!ValidationPolicyCommandInterest.parseCommandInterest_
+        (interest, state, keyName, timestamp))
+      return;
+
+    if (!this.checkTimestamp_(state, keyName[0], timestamp[0]))
+      return;
+
+    this.getInnerPolicy().checkPolicy(interest, state, continueValidation);
+  }
+};
+
+/**
+ * Set the offset when insertNewRecord_() and cleanUp_() get the current time,
+ * which should only be used for testing.
+ * @param {number} nowOffsetMilliseconds The offset in milliseconds.
+ */
+ValidationPolicyCommandInterest.prototype.setNowOffsetMilliseconds_ = function
+  (nowOffsetMilliseconds)
+{
+  this.nowOffsetMilliseconds_ = nowOffsetMilliseconds;
+};
+
+/**
+ * @param {Name} keyName
+ * @param {number} timestamp
+ * @param {number} lastRefreshed
+ * @constructor
+ */
+ValidationPolicyCommandInterest.LastTimestampRecord =
+  function ValidationPolicyCommandInterestLastTimestampRecord
+  (keyName, timestamp, lastRefreshed)
+{
+  // Copy the Name.
+  this.keyName_ = new Name(keyName);
+  this.timestamp_ = timestamp;
+  this.lastRefreshed_ = lastRefreshed;
+};
+
+ValidationPolicyCommandInterest.prototype.cleanUp_ = function()
+{
+  // nowOffsetMilliseconds_ is only used for testing.
+  var now = new Date().getTime() + this.nowOffsetMilliseconds_;
+  var expiring = now - this.options_.recordLifetime_;
+
+  while ((this.container_.length > 0 &&
+           this.container_[0].lastRefreshed_ <= expiring) ||
+         (this.options_.maxRecords_ >= 0 &&
+           this.container_.length > this.options_.maxRecords_))
+    this.container_.shift();
+};
+
+/**
+ * Get the keyLocatorName and timestamp from the command interest.
+ * @param {Interest} interest The Interest to parse.
+ * @param {ValidationState} state On error, this calls state.fail and returns
+ * false.
+ * @param {Array<Name>} keyLocatorName Set keyLocatorName[0] to the KeyLocator
+ * name.
+ * @param {Array<number>} timestamp Set timestamp[0] to the timestamp as
+ * milliseconds since Jan 1, 1970 UTC.
+ * @return {boolean} On success, return true. On error, call state.fail and
+ * return false.
+ */
+ValidationPolicyCommandInterest.parseCommandInterest_ = function
+  (interest, state, keyLocatorName, timestamp)
+{
+  keyLocatorName[0] = new Name();
+  timestamp[0] = 0;
+
+  var name = interest.getName();
+  if (name.size() < CommandInterestSigner.MINIMUM_SIZE) {
+    state.fail(new ValidationError(ValidationError.POLICY_ERROR,
+      "Command interest name `" + interest.getName().toUri() + "` is too short"));
+    return false;
+  }
+
+  timestamp[0] = name.get(CommandInterestSigner.POS_TIMESTAMP).toNumber();
+
+  keyLocatorName[0] = ValidationPolicy.getKeyLocatorName(interest, state);
+  if (state.isOutcomeFailed())
+    // Already failed.
+    return false;
+
+  return true;
+};
+
+/**
+ * @param {ValidationState} state On error, this calls state.fail and returns
+ * false.
+ * @param {Name} keyName The key name.
+ * @param {number} timestamp The timestamp as milliseconds since Jan 1, 1970 UTC.
+ * @return {boolean} On success, return true. On error, call state.fail and
+ * return false.
+ */
+ValidationPolicyCommandInterest.prototype.checkTimestamp_ = function
+  (state, keyName, timestamp)
+{
+  this.cleanUp_();
+
+  // nowOffsetMilliseconds_ is only used for testing.
+  var now = new Date().getTime() + this.nowOffsetMilliseconds_;
+  if (timestamp < now - this.options_.gracePeriod_ ||
+      timestamp > now + this.options_.gracePeriod_) {
+    state.fail(new ValidationError(ValidationError.POLICY_ERROR,
+      "Timestamp is outside the grace period for key " + keyName.toUri()));
+    return false;
+  }
+
+  var index = this.findByKeyName_(keyName);
+  if (index >= 0) {
+    if (timestamp <= this.container_[index].timestamp_) {
+      state.fail(new ValidationError(ValidationError.POLICY_ERROR,
+        "Timestamp is reordered for key " + keyName.toUri()));
+      return false;
+    }
+  }
+
+  var thisPolicy = this;
+  state.addSuccessCallback
+    (function(interest) {
+      thisPolicy.insertNewRecord_(interest, keyName, timestamp);
+    });
+
+  return true;
+};
+
+/**
+ * @param {Interest} interest
+ * @param {Name} keyName
+ * @param {number} timestamp
+ */
+ValidationPolicyCommandInterest.prototype.insertNewRecord_ = function
+  (interest, keyName, timestamp)
+{
+  // nowOffsetMilliseconds_ is only used for testing.
+  var now = new Date().getTime() + this.nowOffsetMilliseconds_;
+  var newRecord = new ValidationPolicyCommandInterest.LastTimestampRecord
+    (keyName, timestamp, now);
+
+  var index = this.findByKeyName_(keyName);
+  if (index >= 0)
+    // Remove the existing record so we can move it to the end.
+    this.container_.splice(index, 1);
+
+  this.container_.push(newRecord);
+};
+
+/**
+ * Find the record in container_ which has the keyName.
+ * @param {Name} keyName The key name to search for.
+ * @return {number} The index in container_ of the record, or -1 if not found.
+ */
+ValidationPolicyCommandInterest.prototype.findByKeyName_ = function(keyName)
+{
+  for (var i = 0; i < this.container_.length; ++i) {
+    if (this.container_[i].keyName_.equals(keyName))
+      return i;
+  }
+
+  return -1;
 };
 /**
  * Copyright (C) 2018 Regents of the University of California.
