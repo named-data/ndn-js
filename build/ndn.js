93f9b127c6b33d1232b5f49f9b0ac50a76ba4ca1
Jeff Thompson <jefft0@remap.ucla.edu>
2018-09-23 08:00:13 -0700

    ndn.js: Rebuild to get ndn.noConflict().

--- build/ndn.js
+++ build/ndn.js
@@ -509,6 +509,7 @@
 
     return printStackTrace;
 }));
+(function(globals){
 /**
  * Copyright (C) 2014-2018 Regents of the University of California.
  * @author: Ryan Bennett
@@ -10657,7 +10658,8 @@ TlvDecoder.prototype.readNestedTlvsStart = function(expectedType)
  * unrecognized type code to skip is critical, throw an exception. If true, then
  * skip the unrecognized type code without error.
  * @throws DecodingException if the TLV length does not equal the total length
- * of the nested TLVs.
+ * of the nested TLVs, or if skipCritical is false and the unrecognized type
+ * code to skip is critical.
  */
 TlvDecoder.prototype.finishNestedTlvs = function(endOffset, skipCritical)
 {
@@ -53890,6 +53892,35 @@ FireflyFace.prototype.establishDocumentPromise_ = function(name)
 
   return establish(this.db_.collection(name.get(0).toEscapedString()), 0);
 };
+var _savedGlobals = {};
+Object.keys(ndn).forEach(function(k){
+  _savedGlobals[k] = globals[k];
+  globals[k] = ndn[k];
+});
+
+/**
+ * Stop exposing ndn-js class names in the global namespace.
+ *
+ * By default, ndn-js exposes all class names into the global namespace for convenience.
+ * This function removes ndn-js class names from the global namespace to avoid conflicts.
+ * After that, the application must explicitly reference `ndn.` before a class name.
+ */
+ndn.noConflict = function() {
+  Object.keys(_savedGlobals).forEach(function(k){
+    if (globals[k] === ndn[k]) {
+      if (typeof _savedGlobals[k] == 'undefined') {
+        delete globals[k];
+      }
+      else {
+        globals[k] = _savedGlobals[k];
+      }
+    }
+  });
+  return ndn;
+};
+
+globals.ndn = ndn;
+})(this);
 /* Minimalistic IndexedDB Wrapper with Bullet Proof Transactions
    =============================================================
 
